3e40c3352ec15639fa88910f2c24171805d8fa5b
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-10-18 17:47:15 +0200

    add funzioni e controllo

--- pari o dispari.js
+++ pari o dispari.js
@@ -1,27 +1,70 @@
-console.log("Pari o dispari")
-
-// Numero random da 1 a 5 per per il PC
+console.log("Pari o dispari?")
+// 
+// Funzione => Numero random da 1 a 5 per per il PC
 function randomNumberFrom1to5(num1_5) {
   let num = Math.floor(Math.random() * 5) + 1
   return num
 }
-let choose = prompt ("Scegli pari o dispari")
-if (choose === 'pari' || 'dispari') {
-  console.log (choose)
-  let numUser = parseInt(prompt("Inserisci un numero da 1 a 5"))
-  if (numUser <= 5) {
-    console.log(`numero dell'utente: ${numUser}`)
-    let numPc = randomNumberFrom1to5()
-    console.log(`numero del pc: ${numPc}`)
-  } else {
-    alert("Errore")
-  }
 
 
+// Sccegli pari o dispari
+let choose = prompt("Scegli pari o dispari")
+choose = choose.toLocaleLowerCase()
+
+// Controllo Pari o dispari
+if (choose === "pari" || choose === "dispari") {
+  console.log("Hai scelto: " + choose)
 
 } else {
-  alert ("Errore")
+  alert("Errore")
 }
+
+// Numero generato dall'utente
+let numUser = parseInt(prompt("Inserisci un numero da 1 a 5"))
+if (numUser <= 5) {
+  console.log(`Numero dell'utente: ${numUser}`)
+ 
+} else {
+  alert("Errore")
+}
+
+// Numero generato dal pc
+let numPc = randomNumberFrom1to5()
+console.log(`Numero del pc: ${numPc}`)
+
+//Somma dei numeri
+let sum = numUser + numPc
+
+// Funzione se è pari
+function isEven(numero) {
+  if (sum % 2 === 0) {
+      return true
+  } else {
+      return false
+  }
+}
+
+let result = isEven(sum)
+
+// Controllo funzione is Even
+if (result === true) {
+  result = "pari"
+  console.log(`La somma è ${sum} ed è ${result}`)
+ 
+} else {
+  result = "dispari"
+  console.log(`La somma è ${sum} ed è ${result}`)
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
